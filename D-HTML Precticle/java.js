326c8ef715daa3037cf29a543b660e05efe201ab
keyurrakholiya75 <rakholiyakeyur16072@gmail.com>
2022-02-20 23:06:32 +0530

    Coad

---
+++ D-HTML Precticle/java.js
@@ -0,0 +1,12 @@
+function changeBackgrounguser(){
+document.getElementById("user").style.background="blue";
+}
+//document.body.style.background="red";
+//document.body.style.fontcolor="blue";
+
+var X=5;
+var Y=10;
+var answer=X+Y;
+document.write("<h2>X=5</h2>");
+document.write("<h2>Y=10</h2>");
+document.write("<h2>Answer(X+Y):"+answer);
